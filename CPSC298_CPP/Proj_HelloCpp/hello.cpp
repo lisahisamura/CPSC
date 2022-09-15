633768a7a9c21132259636f46b90a8287e99a2ef
lisahisamura <hisamura@chapman.edu>
2022-09-15 01:31:24 +0000

    updated hello.cpp with comments

--- CPSC298_CPP/Proj_HelloCpp/hello.cpp
+++ CPSC298_CPP/Proj_HelloCpp/hello.cpp
@@ -1,6 +1,11 @@
+/// @file hello.cpp
+/// @brief Outputs Hello C++!
+/// @author Lisa Hisamura hisamura@chapman.edu
+
 #include <iostream>
 int main()
 {
     std::cout << "Hello C++!" << std::endl;
      return 0;
 }
+
