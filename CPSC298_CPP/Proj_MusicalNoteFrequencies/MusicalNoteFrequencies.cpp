46385f8b3e66e619a9bb451794b11fc70f2fcc6b
lisahisamura <hisamura@chapman.edu>
2022-09-21 07:08:18 +0000

    make changes to musical note frequencies program

--- CPSC298_CPP/Proj_MusicalNoteFrequencies/MusicalNoteFrequencies.cpp
+++ CPSC298_CPP/Proj_MusicalNoteFrequencies/MusicalNoteFrequencies.cpp
@@ -6,7 +6,7 @@
 #include <cmath>
 using namespace std;
 
-//to find k
+//function to find k
 int findk (string musicalNote) {
     int k;
     
@@ -36,56 +36,15 @@ int findk (string musicalNote) {
         k = 11;
     }
 
-    // switch(musicalNote){
-    //     case 'C':
-    //         k = 0;
-    //         break;
-    //     case 'C#':
-    //     case "Db":
-    //         k = 1;
-    //         break;
-    //     case "D":
-    //         k = 2;
-    //         break;
-    //     case "D#":
-    //     case "Eb":
-    //         k = 3;
-    //         break;
-    //     case "E":
-    //         k = 4;
-    //         break;
-    //     case "F":
-    //         k = 5;
-    //         break;
-    //     case "F#":
-    //     case "Gb":
-    //         k = 6;
-    //         break;
-    //     case "G":
-    //     case "Ab":
-    //         k = 7;
-    //         break;
-    //     case "G#":
-    //         k = 8;
-    //         break;
-    //     case "A":
-    //         k = 9;
-    //         break;
-    //     case "A#":
-    //     case "Bb":
-    //         k = 10;
-    //         break;
-    //     case "B":
-    //         k = 11;
-    //         break;
-    //     default:
-    //         break;
-    // }
-
     return k;
 }
 
+
+// function to calculate the frequency of a musical note
 double CalcNoteFrequency(string musicalNote, int v, int k) {
+    
+    /* The constant variables below were not used at all in the program yet are included
+    because they were specified in the project instructions. */ 
     const double dTwelfthRootOfTwo = 1.059463094359;
     const double referenceFrequency = 16.35; //C0
 
@@ -96,14 +55,18 @@ double CalcNoteFrequency(string musicalNote, int v, int k) {
     double frequency;
     double wavelengthInMeters;
 
+    // formula
     frequency = referenceFrequency * pow(2, v) * pow(dTwelfthRootOfTwo, k);
 
     return frequency;
 }
 
+
+// function to calculate the wavelength of a musical note
 double CalcNoteWavelength(double frequency) {
     const double speedOfSound = 345;
 
+    //formula 
     double wavelength = speedOfSound/frequency;
 }
 
@@ -121,16 +84,9 @@ int main() {
     double frequencyAnswer = CalcNoteFrequency(musicalNote, v, k);
     double wavelengthAnswerInCentimeters = CalcNoteWavelength(frequencyAnswer) * 100;
 
-
+    // output
     cout << "Reference Frequency: 16.35Hz" << endl;
     cout << "Speed of Sound: 345 m/s" << endl;
-    // cout << "Note: C0; nu: 0; k: 0; frequency: " << frequency << " Hz; wavelength: " << wavelength << " cm" << endl;
-    // cout << "Note: C#0; nu: 0; k: 1; frequency: " << frequency << " Hz; wavelength: " << wavelength << " cm" << endl;
-    // cout << "Note: D0; nu: 0; k: 2; frequency: " << frequency << " Hz; wavelength: " << wavelength << " cm" << endl;
-    // cout << "Note: D3; nu: 3; k: 2; frequency: " << frequency << " Hz; wavelength: " << wavelength << " cm" << endl;
-    // cout << "Note: C4; nu: 4; k: 0; frequency: " << frequency << " Hz; wavelength: " << wavelength << " cm" << endl;
-    // cout << "Note: D#7; nu: 7; k: 3; frequency: " << frequency << " Hz; wavelength: " << wavelength << " cm" << endl;
-    // cout << "Note: C8; nu: 8; k: 0; frequency: " << frequency << " Hz; wavelength: " << wavelengthInMeters * 100 << " cm" << endl;
 
     cout << "Note: " << musicalNote << v << "; nu: " << v << "; k: " << k << "; frequency: " << frequencyAnswer << " Hz; wavelength: " << wavelengthAnswerInCentimeters << " cm" << endl;
 
